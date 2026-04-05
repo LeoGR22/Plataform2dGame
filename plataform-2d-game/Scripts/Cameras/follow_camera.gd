c5f4d600226b5080aa4b8963fb219badae59ac2d
LeoGR22 <leonardogr2212@gmail.com>
2026-04-05 14:10:09 -0300

    Camera que segue o jogador
    Camera que segue a posição do jogador

---
+++ plataform-2d-game/Scripts/Cameras/follow_camera.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var move_speed = 5.0 #velocidade da suavização
+var height = 12
+
+func _process(delta):
+	var players = get_tree().get_nodes_in_group("players")
+	
+	if players.size() > 0:
+		var target_pos = Vector2.ZERO
+		
+		for player in players:
+			target_pos += player.global_position
+		target_pos /= players.size()
+		target_pos.y -= height
+		
+		global_position = global_position.lerp(target_pos, move_speed * delta)
